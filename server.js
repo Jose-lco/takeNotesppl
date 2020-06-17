3162877f8136b5e39f93cbefc704b466af1d400b
Jose-lco <ndungu642@gmail.com>
2020-06-16 18:45:40 -0700

    connecting the html with the js file in the front-end

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const uuid = require('uuid');
 const app = express();
 const PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/public'));
 app.use(express.json());
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public','index.html'));
